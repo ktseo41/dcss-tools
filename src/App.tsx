9e9996b22209a9d85859c35cef9aa79125d4cdab
bohyeon <ktseo41@gmail.com>
2025-02-01 21:32:37 +0900

    feat: 컨텐츠를 중앙 정렬하고 탭 내부 영역 너비를 지정한다

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 
 function App() {
   return (
-    <div className="p-4">
-      <Tabs defaultValue="ev">
-        <TabsList>
+    <div className="p-4 flex items-center justify-center w-screen">
+      <Tabs defaultValue="ev" className="w-full max-w-2xl">
+        <TabsList className="w-full">
           <TabsTrigger value="ev">EV Calculator</TabsTrigger>
           <TabsTrigger value="armour">Armour Calculator</TabsTrigger>
         </TabsList>
