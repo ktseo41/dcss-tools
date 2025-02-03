fdd0deffc15fb064f2f2f769e5ee35dbf72d8a46
bohyeon <ktseo41@gmail.com>
2025-02-03 16:07:11 +0900

    feat: bbs style을 적용한다

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,11 @@ function App() {
   return (
     <div className="p-4 flex items-center justify-center w-screen">
       <Tabs defaultValue="ev" className="w-full max-w-2xl">
-        <TabsList className="w-full">
+        {/* outline: none이 inline style로 적용되어 있어 className이 아닌 style attr를 통해 덮어씌운다 */}
+        <TabsList
+          className="w-full gap-x-2"
+          style={{ outline: "1px solid white", outlineOffset: "-4px" }}
+        >
           <TabsTrigger value="ev">EV Calculator</TabsTrigger>
           <TabsTrigger value="ac">AC Calculator</TabsTrigger>
         </TabsList>
--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -2,15 +2,7 @@ import { useEffect } from "react";
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   useEffect(() => {
-    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-
-    const updateTheme = (e: MediaQueryListEvent | MediaQueryList) => {
-      document.documentElement.classList.toggle("dark", e.matches);
-    };
-
-    updateTheme(mediaQuery);
-    mediaQuery.addEventListener("change", updateTheme);
-    return () => mediaQuery.removeEventListener("change", updateTheme);
+    document.documentElement.classList.add("dark");
   }, []);
 
   return children;
