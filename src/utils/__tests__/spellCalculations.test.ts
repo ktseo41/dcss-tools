7fd1b0acb1c29abf8da95931c8dd4455d39b3bc8
bohyeon <ktseo41@gmail.com>
2025-02-07 11:11:09 +0900

    feat: chance2 계산을 c++ 에 맞춰 수정

--- src/utils/__tests__/spellCalculations.test.ts
+++ src/utils/__tests__/spellCalculations.test.ts
@@ -55,11 +55,11 @@ function calculateSpellPenalty({
 
   // 최종 패널티 계산
   const totalPenalty = calculateArmourPenalty() + calculateShieldPenalty();
-  return Math.max(totalPenalty / SCALE, 0);
+  return Math.max(totalPenalty, 0) / SCALE;
 }
 
 function tetrahedralNumber(n: number) {
-  return (n * (n + 1) * (n + 2)) / 6;
+  return Math.floor((n * (n + 1) * (n + 2)) / 6);
 }
 
 function getTrueFailRate(rawFail: number) {
@@ -86,18 +86,12 @@ function failureRateToInt(fail: number) {
   else return Math.max(1, Math.trunc(100 * getTrueFailRate(fail)));
 }
 
-// function failureRateToInt(fail: number) {
-//   if (fail <= 0) return 0;
-//   else if (fail >= 100) return Math.floor((fail + 100) / 2);
-//   else return Math.max(1, Math.trunc(100 * getTrueFailRate(fail)));
-// }
-
 function rawSpellFail({
   playerStrength,
   playerSpellcasting,
   intelligence,
   conjurationSkill,
-  fireSkill,
+  secondSkill,
   spellDifficulty,
   equippedArmour,
   equippedShield,
@@ -110,7 +104,7 @@ function rawSpellFail({
   // 주문 기술력 계산
   const skillCount = 2; // conjuration + fire
   const spellPower =
-    (((conjurationSkill * 200 + fireSkill * 200) / skillCount +
+    (((conjurationSkill * 200 + secondSkill * 200) / skillCount +
       playerSpellcasting * 50) *
       6) /
     100;
@@ -139,7 +133,7 @@ function rawSpellFail({
 
   // 3차 다항식을 통한 실패율 계산
   const chance2 = Math.max(
-    (((chance + 426) * chance + 82670) * chance + 7245398) / 262144,
+    Math.floor((((chance + 426) * chance + 82670) * chance + 7245398) / 262144),
     0
   );
 
@@ -149,118 +143,25 @@ function rawSpellFail({
   return failureRateToInt(failRate);
 }
 
-// function calculateSpellFailure({
-//   playerStrength,
-//   playerSpellcasting,
-//   intelligence,
-//   conjurationSkill,
-//   fireSkill,
-//   spellDifficulty,
-//   equippedArmour,
-//   equippedShield,
-//   armourSkill,
-//   shieldSkill,
-// }: SpellCalculationParams) {
-//   // 1. Spell skills 계산
-//   const spellSkills =
-//     playerSpellcasting / 2 + ((conjurationSkill + fireSkill) * 2) / 2;
-
-//   // 2. Spell difficulty
-//   const spellDifficultyValues = [0, 3, 15, 35, 70, 100, 150, 200, 260, 340]; // 0 Level is added for index matching
-//   const spellDifficultyValue = spellDifficultyValues[spellDifficulty];
-
-//   // 3. Encumbrance penalties 계산 (Armour)
-//   const armourEvPenalty =
-//     ((1 / 225) * equippedArmour.encumbrance ** 2 * (90 - 2 * armourSkill)) /
-//     (playerStrength + 3);
-//   const armourSpellPenalty = 19 * armourEvPenalty;
-
-//   // 4. Encumbrance penalties 계산(Shield)
-//   const shieldEvPenalty =
-//     (((2 / 5) * equippedShield.encumbrance ** 2) / (5 + playerStrength)) *
-//     ((27 - shieldSkill) / 27);
-//   const shieldSpellPenalty = 19 * shieldEvPenalty;
-
-//   // 5. base failure 계산
-//   let spellFailure =
-//     60 -
-//     6 * spellSkills -
-//     2 * intelligence +
-//     spellDifficultyValue +
-//     armourSpellPenalty +
-//     shieldSpellPenalty;
-
-//   // since spellFailure is passed through a 3rd degree polynomial, cap the
-//   // value to avoid any overflow issues. We choose 210 by solving for chance2
-//   // = 200 in the polynomial -- it gets capped at 100 ultimately, but we
-//   // need a bunch of headroom in case some later calculations lower the value
-//   // below 100 after this.
-//   spellFailure = Math.min(spellFailure, 210);
-
-//   // 6. Stepdown 계산
-//   let spellFailureStepped;
-//   spellFailureStepped = Math.max(
-//     (((spellFailure + 426) * spellFailure + 82670) * spellFailure + 7245398) /
-//       262144,
-//     0
-//   );
-
-//   //   if (spellFailure < 43) {
-//   //     spellFailureStepped =
-//   //       ((spellFailure ** 3 + 426 * spellFailure ** 2 + 82670 * spellFailure + 6983254) /
-//   //         262144);
-//   //   } else {
-//   //     spellFailureStepped = spellFailure;
-//   //   }
-//   // }
-//   // 7. modifiers (일단 없음)
-
-//   //8. final step ( sigmoid function 근사)
-//   /*   let realFailureRate;
-//   const n = spellFailureStepped * 3;
-//   realFailureRate = (n * (n + 1) * (n + 2)) / 6 / 101 / 101 * 100; */
-//   spellFailureStepped = Math.min(spellFailureStepped, 100);
-
-//   return spellFailureStepped;
-// }
-
 describe("Spell Calculations", () => {
-  // test("should calculate spell success correctly", () => {
-  //   const params = {
-  //     playerStrength: 4,
-  //     playerSpellcasting: 4.5,
-  //     intelligence: 25,
-  //     conjurationSkill: 5.3,
-  //     fireSkill: 1.4,
-  //     spellDifficulty: 4 as SpellDifficultyLevel,
-  //     equippedArmour: { encumbrance: 0 },
-  //     equippedShield: { encumbrance: 0 },
-  //     armourSkill: 0,
-  //     shieldSkill: 0,
-  //   };
+  test("should calculate spell success correctly", () => {
+    const params = {
+      playerStrength: 4,
+      playerSpellcasting: 4.5,
+      intelligence: 25,
+      conjurationSkill: 5.3,
+      secondSkill: 0,
+      spellDifficulty: 4 as SpellDifficultyLevel,
+      equippedArmour: { encumbrance: 0 },
+      equippedShield: { encumbrance: 0 },
+      armourSkill: 0,
+      shieldSkill: 0,
+    };
 
-  //   const successRate = calculateSpellSuccess(params);
-  //   expect(successRate).toBe(100);
-  // });
-  //
-  // test("should calculate spell success correctly", () => {
-  //   const params = {
-  //     playerStrength: 4,
-  //     playerSpellcasting: 4.5,
-  //     intelligence: 25,
-  //     conjurationSkill: 5.3,
-  //     fireSkill: 0,
-  //     spellDifficulty: 4 as SpellDifficultyLevel,
-  //     equippedArmour: { encumbrance: 0 },
-  //     equippedShield: { encumbrance: 0 },
-  //     armourSkill: 0,
-  //     shieldSkill: 0,
-  //   };
+    const failureRate = rawSpellFail(params);
 
-  //   const failureRate = rawSpellFail(params);
-
-  //   expect(failureRate).toBe(28);
-  // });
+    expect(failureRate).toBe(28);
+  });
 
   test("should calculate spell success correctly", () => {
     const params = {
@@ -268,7 +169,7 @@ describe("Spell Calculations", () => {
       playerSpellcasting: 4.8,
       intelligence: 25,
       conjurationSkill: 5.4,
-      fireSkill: 0.8,
+      secondSkill: 0.8,
       spellDifficulty: 4 as SpellDifficultyLevel,
       equippedArmour: { encumbrance: 0 },
       equippedShield: { encumbrance: 0 },
@@ -287,7 +188,7 @@ describe("Spell Calculations", () => {
       playerSpellcasting: 6.2,
       intelligence: 31,
       conjurationSkill: 6.3,
-      fireSkill: 3.2,
+      secondSkill: 3.2,
       spellDifficulty: 4 as SpellDifficultyLevel,
       equippedArmour: { encumbrance: 0 },
       equippedShield: { encumbrance: 0 },
@@ -306,7 +207,7 @@ describe("Spell Calculations", () => {
       playerSpellcasting: 9.2,
       intelligence: 32,
       conjurationSkill: 7.2,
-      fireSkill: 4.3,
+      secondSkill: 4.3,
       spellDifficulty: 4 as SpellDifficultyLevel,
       equippedArmour: { encumbrance: 0 },
       equippedShield: { encumbrance: 0 },
@@ -325,7 +226,7 @@ describe("Spell Calculations", () => {
       playerSpellcasting: 9.2,
       intelligence: 32,
       conjurationSkill: 7.2,
-      fireSkill: 4.3,
+      secondSkill: 4.3,
       spellDifficulty: 4 as SpellDifficultyLevel,
       equippedArmour: { encumbrance: 4 },
       equippedShield: { encumbrance: 0 },
@@ -344,7 +245,7 @@ describe("Spell Calculations", () => {
       playerSpellcasting: 9.2,
       intelligence: 32,
       conjurationSkill: 7.2,
-      fireSkill: 4.3,
+      secondSkill: 4.3,
       spellDifficulty: 4 as SpellDifficultyLevel,
       equippedArmour: { encumbrance: 14 },
       equippedShield: { encumbrance: 0 },
@@ -363,7 +264,7 @@ describe("Spell Calculations", () => {
       playerSpellcasting: 9.2,
       intelligence: 32,
       conjurationSkill: 7.2,
-      fireSkill: 4.3,
+      secondSkill: 4.3,
       spellDifficulty: 4 as SpellDifficultyLevel,
       equippedArmour: { encumbrance: 7 },
       equippedShield: { encumbrance: 0 },
@@ -382,7 +283,7 @@ describe("Spell Calculations", () => {
       playerSpellcasting: 10.3,
       intelligence: 34,
       conjurationSkill: 8,
-      fireSkill: 5,
+      secondSkill: 5,
       spellDifficulty: 4 as SpellDifficultyLevel,
       equippedArmour: { encumbrance: 0 },
       equippedShield: { encumbrance: 0 },
@@ -393,5 +294,45 @@ describe("Spell Calculations", () => {
     const failureRate = rawSpellFail(params);
 
     expect(failureRate).toBe(1);
+  });
+
+  // https://archive.nemelex.cards/morgue/caiman/morgue-caiman-20250131-054317.txt
+  test("should calculate spell success correctly", () => {
+    const params = {
+      playerStrength: 13,
+      playerSpellcasting: 7,
+      intelligence: 13,
+      conjurationSkill: 8,
+      secondSkill: 4,
+      spellDifficulty: 3 as SpellDifficultyLevel,
+      equippedArmour: { encumbrance: 4 },
+      equippedShield: { encumbrance: 0 },
+      armourSkill: 9,
+      shieldSkill: 0,
+    };
+
+    const failureRate = rawSpellFail(params);
+
+    expect(failureRate).toBe(4);
+  });
+
+  // https://archive.nemelex.cards/morgue/caiman/morgue-caiman-20250126-091458.txt
+  test("should calculate spell success correctly", () => {
+    const params = {
+      playerStrength: 41,
+      playerSpellcasting: 15,
+      intelligence: 9,
+      conjurationSkill: 14,
+      secondSkill: 6,
+      spellDifficulty: 4 as SpellDifficultyLevel,
+      equippedArmour: { encumbrance: 15 },
+      equippedShield: { encumbrance: 10 },
+      armourSkill: 15,
+      shieldSkill: 18,
+    };
+
+    const failureRate = rawSpellFail(params);
+
+    expect(failureRate).toBe(4);
   });
 });
