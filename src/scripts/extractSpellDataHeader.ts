e6f5b4c936423d7056ad64d38f35df7ffbcdaa40
bohyeon <ktseo41@gmail.com>
2025-02-10 21:59:21 +0900

    feat: spell 추출 refactor, filter 추가
    - 테스트를 위해 parseSpellBlock을 내보냄
    - 플레이어 전용 주문을 추출하기 위해 몬스터, 지팡이, 플레이어가 마법으로서 사용하는 것이 아닌 주문을 삭제

--- src/scripts/extractSpellDataHeader.ts
+++ src/scripts/extractSpellDataHeader.ts
@@ -1,4 +1,4 @@
-import fs from "fs";
+import * as fs from "fs";
 
 type SpellData_PROTO = {
   id: string;
@@ -15,7 +15,42 @@ type SpellData_PROTO = {
   tile: string;
 };
 
-function extractSpellData(fileContent: string) {
+const wandSpells = {
+  WAND_FLAME: "SPELL_THROW_FLAME",
+  WAND_PARALYSIS: "SPELL_PARALYSE",
+  WAND_DIGGING: "SPELL_DIG",
+  WAND_ICEBLAST: "SPELL_ICEBLAST",
+  WAND_POLYMORPH: "SPELL_POLYMORPH",
+  WAND_CHARMING: "SPELL_CHARMING",
+  WAND_ACID: "SPELL_CORROSIVE_BOLT",
+  WAND_LIGHT: "SPELL_BOLT_OF_LIGHT",
+  WAND_QUICKSILVER: "SPELL_QUICKSILVER_BOLT",
+  WAND_MINDBURST: "SPELL_MINDBURST",
+  WAND_ROOTS: "SPELL_FASTROOT",
+  WAND_WARPING: "SPELL_WARP_SPACE",
+}
+
+const playerNonbookSpells = [
+  "SPELL_THUNDERBOLT",  
+  "SPELL_PHANTOM_MIRROR",
+  "SPELL_TREMORSTONE",
+  "SPELL_GRAVITAS",
+  "SPELL_SONIC_WAVE",
+  "SPELL_SMITING",
+  "SPELL_UNLEASH_DESTRUCTION",
+  "SPELL_HURL_TORCHLIGHT",
+  "SPELL_HURL_DAMNATION",
+  "SPELL_NOXIOUS_BREATH",
+  "SPELL_COMBUSTION_BREATH",
+  "SPELL_GLACIAL_BREATH",
+  "SPELL_NULLIFYING_BREATH",
+  "SPELL_STEAM_BREATH",
+  "SPELL_CAUSTIC_BREATH",
+  "SPELL_GALVANIC_BREATH",
+  "SPELL_MUD_BREATH",
+]
+
+const extractSpellData = (fileContent: string) => {
   try {
     // 시작 부분을 찾을 때 더 유연하게 검색
     const startMarker = "spelldata[] =";
@@ -60,15 +95,7 @@ function extractSpellData(fileContent: string) {
   }
 }
 
-function parseSpellData(spellDataSection: string) {
-  const spells: SpellData_PROTO[] = [];
-  // 각각의 spell 블록을 매칭하는 정규식
-  const spellRegex = /{\s*SPELL_[^}]+}/g;
-
-  const matches = spellDataSection.match(spellRegex);
-  if (!matches) return spells;
-
-  matches.forEach((spellBlock) => {
+export const parseSpellBlock = (spellBlock: string): SpellData_PROTO | undefined => {
     // AXED_SPELL 체크
     if (spellBlock.includes("AXED_SPELL")) {
       return; // skip this iteration
@@ -97,21 +124,31 @@ function parseSpellData(spellDataSection: string) {
         tile: lines[9] ? lines[9].trim() : "",
       };
 
-      spells.push(spell);
+      return spell;
     } catch (error) {
       console.error("Error parsing spell block:", spellBlock, error);
     }
-  });
-
-  return spells;
 }
 
-function extractSpellName(nameStr: string) {
+const parseSpellData = (spellDataSection: string) => {
+  const spells: [] = [];
+  // 각각의 spell 블록을 매칭하는 정규식
+  const spellRegex = /{\s*SPELL_[^}]+}/g;
+
+  const matches = spellDataSection.match(spellRegex);
+  if (!matches) return spells;
+
+  const result = matches.map(parseSpellBlock).filter(p => p !== undefined);
+
+  return result;
+}
+
+const extractSpellName = (nameStr: string) => {
   const matches = nameStr.match(/"([^"]+)"/);
   return matches ? matches[1] : nameStr.trim();
 }
 
-function parseSchools(schoolsStr: string) {
+const parseSchools = (schoolsStr: string) => {
   if (!schoolsStr) return [];
   return schoolsStr
     .split("|")
@@ -119,13 +156,76 @@ function parseSchools(schoolsStr: string) {
     .filter((s) => s !== "none");
 }
 
-function parseFlags(flagsStr: string) {
+const parseFlags = (flagsStr: string) => {
   if (!flagsStr) return [];
   return flagsStr
     .split("|")
     .map((f) => f.trim().replace("spflag::", ""))
     .filter((f) => f !== "none");
 }
+
+const extractSets = (filePath: string) => {
+  const fileContent = fs.readFileSync(filePath, "utf8");
+  const spells = JSON.parse(fileContent);
+  const uniqueNames = new Set<string>();
+  const uniqueSchools = new Set<string>();
+  const uniqueFlags = new Set<string>();
+
+  spells.forEach((spell: SpellData_PROTO) => {
+    uniqueNames.add(spell.name);
+    spell.schools.forEach((school) => uniqueSchools.add(school));
+    spell.flags.forEach((flag) => uniqueFlags.add(flag));
+  });
+
+  if (uniqueNames.size === 0) {
+    throw new Error("No unique names found");
+  }
+
+  if (uniqueSchools.size === 0) {
+    throw new Error("No unique schools found");
+  }
+
+  if (uniqueFlags.size === 0) {
+    throw new Error("No unique flags found");
+  }
+
+  return {
+    uniqueNames,
+    uniqueSchools,
+    uniqueFlags,
+  };
+}
+
+const makeTypeDefinition = (sets: {
+  uniqueNames: Set<string>;
+  uniqueSchools: Set<string>;
+  uniqueFlags: Set<string>;
+}) => {
+  const names = Array.from(sets.uniqueNames);
+  const schools = Array.from(sets.uniqueSchools).map(school => school.replace(/(\r\n|\n|\r)/gm, "").trim());
+  const flags = Array.from(sets.uniqueFlags).map(flag => flag.replace(/(\r\n|\n|\r)/gm, "").trim());
+
+  return `
+  export type SpellName = ${names.map((name) => `"${name}"`).join(" | ")};
+  export type School = ${schools.map((school) => `"${school}"`).join(" | ")};
+  export type Flag = ${flags.map((flag) => `"${flag}"`).join(" | ")};
+
+  export type SpellData = {
+    id: string;
+    name: SpellName;
+    schools: School[];
+    flags: Flag[];
+    level: number;
+    power: number;
+    range: {
+      min: number;
+      max: number;
+    };
+    noise: number;
+    tile: string;
+  };
+`;
+};
 
 try {
   const fileContent = fs.readFileSync(
@@ -139,14 +239,20 @@ try {
   }
 
   const parsedSpells = parseSpellData(spellDataSection);
-  const sortedSpells = parsedSpells.sort((a, b) =>
-    a.name.localeCompare(b.name)
-  );
-
+  
+  const monsterExcludedSpells = parsedSpells.filter(spell => !spell.flags.includes("monster"));
+  const monsterAndwandSpellExcludedSpells = monsterExcludedSpells.filter(spell => !Object.values(wandSpells).includes(spell.id));
+  const playerNonbookSpellExcludedSpells = monsterAndwandSpellExcludedSpells.filter(spell => !playerNonbookSpells.includes(spell.id));
+  
   fs.writeFileSync(
-    "src/data/parsed-spells.json",
-    JSON.stringify(sortedSpells, null, 2)
+    "src/data/parsed-spells-no-wand-and-monster-and-player-nonbook.json",
+    JSON.stringify(playerNonbookSpellExcludedSpells, null, 2)
   );
+  
+  const sets = extractSets("src/data/parsed-spells.json");
+  const typeDefinition = makeTypeDefinition(sets);
+
+  fs.writeFileSync("src/data/extracted-spell-types.d.ts", typeDefinition);
 } catch (error) {
   console.error("Failed to process spell data:", error);
   process.exit(1);
